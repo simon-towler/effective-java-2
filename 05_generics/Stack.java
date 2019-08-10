07e0a293e89424ecd74d8b10e62db82913e3ee09
simon-towler <simon.towler@icloud.com>
2019-08-10 10:28:41 +0100

    Worked around prohibition on generic array creation by creating an array of Object and cast to generic array type. Suppressed the resulting warning with an annotation.

--- 05_generics/Stack.java
+++ 05_generics/Stack.java
@@ -1,6 +1,7 @@
 //Initial attempt to generify Stack - won't compile!
 
 import java.util.Arrays;
+import java.util.EmptyStackException;
 
 public class Stack<E> {
     //declare store for the stack elems
@@ -16,9 +17,13 @@ public class Stack<E> {
      * Constructs a new Stack object instance
      * with an empty array of Object elements of the DEFAULT_INITIAL_CAPACITY
      */
+    // The elements array will contain only E instances from push(E).
+    // This is sufficient to ensure tyep safety, but the runtime
+    // type of the array won't be E[]; it will always be Object[]!
+    @SuppressWarnings("unchecked")
     public Stack() {
         //init store for stack elems
-        elements = new E[DEFAULT_INITIAL_CAPACITY];
+        elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
     }
 
 
@@ -42,7 +47,7 @@ public class Stack<E> {
      */
     public E pop() {
         if (size == 0) {
-            //throw new EmptyStackException();//TODO add EmptyStackException
+            throw new EmptyStackException();
         }
         E result = elements[--size];
             elements[size] = null; // Eliminate obsolete reference
